0bfce83524b823755bb16733eca3fac2c84d04b4
milovanderlinden <milo@dogodigi.net>
2020-03-30 21:25:18 +0200

    First try, ugly labels

--- src/components/WorldMap.js
+++ src/components/WorldMap.js
@@ -2,9 +2,15 @@ import { Component } from 'preact';
 import { html } from 'htm/preact';
 import { router } from '../router.js';
 import { lockdownsService } from '../services/locksdownsService.js';
+import css from 'csz';
 
 const mapbox_token = 'pk.eyJ1IjoibWlibG9uIiwiYSI6ImNrMGtvajhwaDBsdHQzbm16cGtkcHZlaXUifQ.dJTOE8FJc801TAT0yUhn3g';
 const today = new Date();
+
+const mapStyles = css`
+  canvas {
+    background: #f2fcff;
+  }`;
 
 export class WorldMap extends Component {
   constructor() {
@@ -20,39 +26,57 @@ export class WorldMap extends Component {
   async componentDidMount() {
     const lockdowns = await lockdownsService.getLockdowns();
     const mapData = await (await fetch(new URL('../../data/worldmap.json', import.meta.url))).json();
-
+    let markers = [];
     let map = new window.mapboxgl.Map({
-      accessToken: mapbox_token,
+      //accessToken: mapbox_token,
       container: this.ref,
-      style: 'mapbox://styles/mapbox/light-v10',
+      //style: 'mapbox://styles/mapbox/light-v10',
       center: [this.state.lng, this.state.lat],
       zoom: this.state.zoom
     });
 
-    map.on('load', function() {
-      for (const feature of mapData.features) {
-        if (lockdowns[feature.properties.NAME]) {
-          feature.properties.data = lockdowns[feature.properties.NAME];
-        }
-        feature.properties.color = worldStyle(feature);
+    for (const feature of mapData.features) {
+      if (lockdowns[feature.properties.NAME]) {
+        feature.properties.data = lockdowns[feature.properties.NAME];
       }
-      map.addSource('countries', {
-        type: 'geojson',
-        data: mapData,
-        generateId: true
-      });
+      feature.properties.color = worldStyle(feature);
+      // create a HTML element for each feature
+      //var polygon = turf.multiPolygon(f.geometry.coordinates);
+      //console.log(polygon);
+      var centroid = turf.centroid(feature.geometry);
+      var el = document.createElement('div');
+      el.className = 'marker';
+      el.innerHTML = feature.properties.NAME;
+      // make a marker for each feature and add to the map
+      markers.push(new mapboxgl.Marker(el).setLngLat(centroid.geometry.coordinates));
+    }
 
-      map.addLayer({
-        id: 'countries',
-        type: 'fill',
-        source: 'countries',
-        layout: {},
-        paint: {
-          'fill-color': ['get', 'color'],
-          'fill-opacity': ['case', ['boolean', ['feature-state', 'hover'], false], 0.8, 0.4]
-        },
-        filter: ['has', 'color']
-      });
+    map.addSource('countries', {
+      type: 'geojson',
+      data: mapData,
+      generateId: true
+    });
+
+    map.addLayer({
+      id: 'countries',
+      type: 'fill',
+      source: 'countries',
+      layout: {},
+      paint: {
+        'fill-color': ['get', 'color'],
+        'fill-opacity': ['case', ['boolean', ['feature-state', 'hover'], false], 0.8, 0.4]
+      },
+      filter: ['has', 'color']
+    });
+    console.log(markers);
+
+    markers.forEach( marker => {
+      marker.addTo(map)
+    });
+  
+
+    map.on('load', function() {
+      
 
       let hoveredStateId = null;
 
@@ -126,7 +150,7 @@ export class WorldMap extends Component {
   render() {
     return html`
       <div
-        class="map-container"
+        class="map-container ${mapStyles}"
         ref=${ref => {
           this.ref = ref;
         }}
