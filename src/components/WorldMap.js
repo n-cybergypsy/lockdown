3fa56252a2f633ee0b40f9ebe41676ba5aaf7cf0
milovanderlinden <milo@dogodigi.net>
2020-03-30 23:29:37 +0200

    Replacing leaflet with Mapbox

--- src/components/WorldMap.js
+++ src/components/WorldMap.js
@@ -2,71 +2,120 @@ import { Component } from 'preact';
 import { html } from 'htm/preact';
 import { router } from '../router.js';
 import { lockdownsService } from '../services/locksdownsService.js';
-
-const mapbox_token = 'pk.eyJ1IjoibWlibG9uIiwiYSI6ImNrMGtvajhwaDBsdHQzbm16cGtkcHZlaXUifQ.dJTOE8FJc801TAT0yUhn3g';
+import css from 'csz';
 const today = new Date();
-
+const mapStyles = css`
+  & {
+    width:100%;
+    height: 100vh;
+  }
+  canvas {
+    background: #f2fcff;
+  }
+`;
 export class WorldMap extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      lng: 0,
+      lat: 0,
+      zoom: 2
+    };
+  }
+
   async componentDidMount() {
     // the world map needs a large data source, lazily fetch them in parallel
     const [lockdowns, mapData, leaflet] = await Promise.all([
       lockdownsService.getLockdowns(),
-      fetch(new URL('../../data/worldmap.json', import.meta.url)).then(r => r.json()),
-      import('leaflet/dist/leaflet-src.esm.js')
+      fetch(new URL('../../data/worldmap.geojson', import.meta.url)).then(r => r.json())
     ]);
-    const { Map, Browser, geoJSON, layerGroup, tileLayer } = leaflet;
 
-    const map = new Map(this.ref, {
-      center: [0, 0],
-      zoom: 3,
-      minZoom: 2,
-      maxZoom: 18,
-      zoomControl: false
+    let markers = [];
+    let map = new window.mapboxgl.Map({
+      container: this.ref,
+      center: [this.state.lng, this.state.lat],
+      zoom: this.state.zoom
     });
-    let themeLayer;
-    let labelLayer = layerGroup();
 
-    function onFeatureClicked(e) {
-      const layer = e.target;
-      // map.fitBounds(layer.getBounds());
-      router.setSearchParam('country', layer.feature.properties.NAME);
-      router.setSearchParam('iso2', layer.feature.properties.iso2);
-    }
-
-    function resetHighlight(e) {
-      const layer = e.target;
-      themeLayer.resetStyle(layer);
-    }
-
-    function highlightFeature(e) {
-      const layer = e.target;
-      layer.setStyle({
-        fillOpacity: 0.5,
-        name: 'test'
-      });
-      if (!Browser.ie && !Browser.opera && !Browser.edge) {
-        layer.bringToFront();
+    for (const feature of mapData.features) {
+      if (lockdowns[feature.properties.NAME]) {
+        feature.properties.data = lockdowns[feature.properties.NAME];
       }
+      feature.properties.color = worldStyle(feature);
+      // create a HTML element for each feature
+      //var polygon = turf.multiPolygon(f.geometry.coordinates);
+      //console.log(polygon);
+      var centroid = turf.centroid(feature.geometry);
+      var el = document.createElement('div');
+      el.className = 'marker';
+      el.innerHTML = feature.properties.NAME;
+      // make a marker for each feature and add to the map
+      markers.push(new mapboxgl.Marker(el).setLngLat(centroid.geometry.coordinates));
     }
 
-    function onEachFeature(feature, layer) {
-      layer.on({
-        mouseover: highlightFeature,
-        mouseout: resetHighlight,
-        click: onFeatureClicked
+    map.addSource('countries', {
+      type: 'geojson',
+      data: mapData,
+      generateId: true
+    });
+
+    map.addLayer({
+      id: 'countries',
+      type: 'fill',
+      source: 'countries',
+      layout: {},
+      paint: {
+        'fill-color': ['get', 'color'],
+        'fill-opacity': ['case', ['boolean', ['feature-state', 'hover'], false], 0.8, 0.4]
+      },
+      filter: ['has', 'color']
+    });
+    console.log(markers);
+
+    markers.forEach(marker => {
+      marker.addTo(map);
+    });
+    
+    map.on('load', function() {
+      let hoveredStateId = null;
+
+      map.on('mousemove', 'countries', function(e) {
+        if (e.features.length > 0) {
+          if (hoveredStateId) {
+            map.setFeatureState(
+              {
+                source: 'countries',
+                id: hoveredStateId
+              },
+              {
+                hover: false
+              }
+            );
+          }
+
+          hoveredStateId = e.features[0].id;
+
+          map.setFeatureState(
+            {
+              source: 'countries',
+              id: hoveredStateId
+            },
+            {
+              hover: true
+            }
+          );
+        }
       });
-    }
-
-    tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token=' + mapbox_token, {
-      tileSize: 512,
-      zoomOffset: -1,
-      attribution:
-        '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
+      map.on('click', 'countries', function(e) {
+        router.setSearchParam('country', e.features[0].properties.NAME);
+        router.setSearchParam('iso2', e.features[0].properties.iso2);
+      });
+    });
 
     function worldStyle(e) {
       // lockdown unknown
-      let value = 'grey';
+      let value = 'orange';
 
       if (e.properties.data && e.properties.data.lockdowns) {
         if (e.properties.data.lockdowns.length === 0) {
@@ -86,38 +135,9 @@ export class WorldMap extends Component {
         }
       }
 
-      let lineOpacity;
-      if (!navigator.onLine) {
-        lineOpacity = 1;
-      } else {
-        lineOpacity = 0.1;
-      }
-
-      const style = {
-        weight: 1,
-        opacity: lineOpacity,
-        color: '#555',
-        fillOpacity: 0
-      };
-
-      if (value) {
-        style.fillColor = value;
-        style.fillOpacity = 0.5;
-      }
-      return style;
+      return value;
     }
 
-    for (const feature of mapData.features) {
-      if (lockdowns[feature.properties.NAME]) {
-        feature.properties.data = lockdowns[feature.properties.NAME];
-      }
-    }
-
-    themeLayer = geoJSON(mapData, {
-      style: worldStyle,
-      onEachFeature: onEachFeature
-    }).addTo(map);
-    labelLayer.addTo(map);
     this.setState({
       map
     });
@@ -154,7 +174,7 @@ export class WorldMap extends Component {
   render() {
     return html`
       <div
-        style="width: 100%"
+      class="map-container ${mapStyles}"
         ref=${ref => {
           this.ref = ref;
         }}
